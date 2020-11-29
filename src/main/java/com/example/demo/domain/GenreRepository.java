d25efd1d2b863d0cfe46940e81f4ec9f68b4dcc3
Shadows <36662635+RoyalBladeV2@users.noreply.github.com>
2020-11-29 20:19:27 +0300

    final touches

--- src/main/java/com/example/demo/domain/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface GenreRepository extends CrudRepository<Genre, Long> {
-    List<Genre> findBygenre(String genre);
-}
